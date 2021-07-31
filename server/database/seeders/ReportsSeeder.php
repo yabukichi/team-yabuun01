0e5936a396a61bf6346e1612ae1f48b24842b125
Hitoshi-Noborikawa <dengchuanrenzhi@gmail.com>
2021-07-31 16:31:18 +0900

    Seederを追加

---
+++ server/database/seeders/ReportsSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use DateTime;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ReportsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            DB::table('reports')->insert([
+                'time' => new DateTime(),
+                'note_id' => $i,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ]);
+        }
+    }
+}
